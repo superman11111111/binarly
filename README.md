f2bdccb11b0581c05c023d3e702ea638bcdb5486
m-kypr <mrckyper@gmail.com>
2020-09-29 23:03:58 +0200

    how am i going to do this?

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Implementation of a binary tree in Python3
+
+TODO:
+* Find format to display binary tree on frontend WITHOUT reimplemtation
+* Write method to convert tree in said format
+* Make pretty is JS
+* Profit
+
+
--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from binarytree import Node
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+  import random
+  n = None
+  for i in range(10):
+    # n = Node(random.randint(0,10), parent=n)
+    n = Node(i, parent=n)
+  print(n.find_root())
+  return str(n)
 
 if __name__ == "__main__":
   app.run(debug=True)
